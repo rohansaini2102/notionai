8b75f9ec9b25b6bba293ac59cf048a54d5c31641
rohansaini2102 <138979578+rohansaini2102@users.noreply.github.com>
2025-03-27 19:38:45 +0530

    final commit before host

--- app/api/createNoteBook/route.tsx
+++ app/api/createNoteBook/route.tsx
@@ -1,56 +1,15 @@
-// // /api/createNoteBook
-
+// /api/createNoteBook
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";      
 import { $notes } from "@/lib/db/schema";
 import { generateImage, generateImagePrompt } from "@/lib/openai";
-// import { auth } from "@clerk/nextjs";
-// import { NextResponse } from "next/server";
 
-// export const runtime = "edge";
-
-// export async function POST(req: Request) {
-//   const { userId } = auth();
-//   if (!userId) {
-//     return new NextResponse("unauthorised", { status: 401 });
-//   }
-//   const body = await req.json();
-//   const { name } = body;
-//   const image_description = await generateImagePrompt(name);
-//   if (!image_description) {
-//     return new NextResponse("failed to generate image description", {
-//       status: 500,
-//     });
-//   }
-//   const image_url = await generateImage(image_description);
-//   if (!image_url) {
-//     return new NextResponse("failed to generate image ", {
-//       status: 500,
-//     });
-//   }
-
-//   const note_ids = await db
-//     .insert($notes)
-//     .values({
-//       name,
-//       userId,
-//       imageUrl: image_url,
-//     })
-//     .returning({
-//       insertedId: $notes.id,
-//     });
-
-//   return NextResponse.json({
-//     note_id: note_ids[0].insertedId,
-//   });
-// }
-
-
+export const runtime = "edge";
 
 export async function POST(req: Request) {
-const { userId } = await auth();
-if(!userId){
+  const { userId } = await auth();
+  if (!userId) {
     return new NextResponse("unauthorised", { status: 401 });
   }
   const body = await req.json();
@@ -83,3 +42,41 @@ if(!userId){
     note_id: note_ids[0].insertedId,
   });
 }
+
+
+
+// export async function POST(req: Request) {
+// const { userId } = await auth();
+// if(!userId){
+//     return new NextResponse("unauthorised", { status: 401 });
+//   }
+//   const body = await req.json();
+//   const { name } = body;
+//   const image_description = await generateImagePrompt(name);
+//   if (!image_description) {
+//     return new NextResponse("failed to generate image description", {
+//       status: 500,
+//     });
+//   }
+//   const image_url = await generateImage(image_description);
+//   if (!image_url) {
+//     return new NextResponse("failed to generate image ", {
+//       status: 500,
+//     });
+//   }
+
+//   const note_ids = await db
+//     .insert($notes)
+//     .values({
+//       name,
+//       userId,
+//       imageUrl: image_url,
+//     })
+//     .returning({
+//       insertedId: $notes.id,
+//     });
+
+//   return NextResponse.json({
+//     note_id: note_ids[0].insertedId,
+//   });
+// }
